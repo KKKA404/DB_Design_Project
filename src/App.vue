78dd415353a12a88584f6f5c69793b42f83668d6
Baokker <1048098862@qq.com>
2022-07-20 13:52:43 +0800

    init checkdonation

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <CheckDonationRecord />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import CheckDonationRecord from './views/CheckDonationRecord.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    // HelloWorld,
+    CheckDonationRecord
+}
 }
 </script>
 
