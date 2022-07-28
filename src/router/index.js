7a5dee011285dd5229782f161bb3b4b93c560d85
Baokker <1048098862@qq.com>
2022-07-28 11:39:01 +0800

    merge router

---
+++ src/router/index.js
@@ -0,0 +1,116 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+
+Vue.use(VueRouter);
+
+// 创建新页面后，在本文件下进行引入即可
+export default new VueRouter({
+  mode: "history",
+  routes: [
+    {
+      path: "/",
+      name: "登录",
+      meta: false,
+      component: () => import("../views/SystemLogin.vue"),
+    },
+    {
+      path: "/Index",
+      name: "物资",
+      component: () => import("../views/SystemIndex.vue"),
+      meta: true,
+      children: [
+        {
+          path: "/CheckDonationRecord",
+          name: "物资捐赠记录查询",
+          component: () => import("../views/CheckDonationRecord.vue"),
+        },
+        {
+          path: "/MakeDonation",
+          name: "捐赠物资",
+          component: () => import("../views/MakeDonation.vue"),
+        },
+      ],
+    },
+    {
+      path: "/Index",
+      name: "个人需求表单",
+      component: () => import("../views/SystemIndex.vue"),
+      meta: true,
+      children: [
+        {
+          path: "/AddRecord",
+          name: "个人需求表单填写",
+          component: () => import("../views/AddRecord.vue"),
+        },
+        {
+          path: "/RecordManage",
+          name: "个人需求表单查询",
+          component: () => import("../views/RecordManage.vue"),
+        },
+      ],
+    },
+    {
+      path: "/Index",
+      name: "现有物资",
+      component: () => import("../views/SystemIndex.vue"),
+      meta: true,
+      children: [
+        {
+          path: "/MaterialManage",
+          name: "现有物资查询",
+          component: () => import("../views/MaterialManage.vue"),
+        },
+      ],
+    },
+    {
+      path: "/Index",
+      name: "物资购买",
+      component: () => import("../views/SystemIndex.vue"),
+      meta: true,
+      children: [
+        {
+          path: "/CheckNeedRecord",
+          name: "物资需求查询",
+          component: () => import("../views/CheckNeedRecord.vue"),
+        },
+        {
+          path: "/CheckPurchRecord",
+          name: "物资购买查询",
+          component: () => import("../views/CheckPurchRecord.vue"),
+        },
+      ],
+    },
+    {
+      path: "/Index",
+      name: "核酸管理",
+      component: () => import("../views/SystemIndex.vue"),
+      meta: true,
+      children: [
+        {
+          path: "/DNAManage",
+          name: "核酸信息管理",
+          component: () => import("../views/DNAManage.vue"),
+        },
+
+        {
+          path: "/RecordPatient",
+          name: "病历记录",
+          component: () => import("../views/RecordPatient.vue"),
+        },
+      ],
+    },
+    {
+      path: "/Index",
+      name: "志愿管理",
+      component: () => import("../views/SystemIndex.vue"),
+      meta: true,
+      children: [
+        {
+          path: "/VolunteerApplication",
+          name: "志愿申请信息",
+          component: () => import("../views/VolunteerApplication.vue"),
+        },
+      ],
+    },
+  ],
+});
--- src/views/SystemIndex.vue
+++ src/views/SystemIndex.vue
@@ -21,10 +21,20 @@ export default {
   <div class="index">
     <el-container style="height: 100%; border: 1px solid #eee">
       <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
-        <el-menu router :default-active='$route.path'>
-          <el-submenu v-for="(item, index) in router" :index="index + ''" :key="index">
-            <template slot="title"><i class="el-icon-setting"></i>{{ item.name }}</template>
-            <el-menu-item v-for="(item2, index2) in item.children" :index="item2.path" :key="index2">{{ item2.name }}
+        <el-menu router :default-active="$route.path">
+          <el-submenu
+            v-for="(item, index) in router"
+            :index="index + ''"
+            :key="index"
+          >
+            <template slot="title"
+              ><i class="el-icon-setting"></i>{{ item.name }}</template
+            >
+            <el-menu-item
+              v-for="(item2, index2) in item.children"
+              :index="item2.path"
+              :key="index2"
+              >{{ item2.name }}
             </el-menu-item>
           </el-submenu>
         </el-menu>
@@ -39,24 +49,22 @@ export default {
           <BottomFooter></BottomFooter>
         </el-footer>
       </el-container>
-
     </el-container>
   </div>
 </template>
 <script>
-
 export default {
   data() {
     return {
-      currentMenu: '',
-      router: ""
-    }
+      currentMenu: "",
+      router: "",
+    };
   },
   created() {
-    this.router = this.$router.options.routes.filter(i => {
-      return i.meta
-    })
+    this.router = this.$router.options.routes.filter((i) => {
+      return i.meta;
+    });
   },
-  name: 'SystemIndex',
-}
+  name: "SystemIndex",
+};
 </script>
