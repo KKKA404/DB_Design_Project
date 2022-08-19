40f08c37d3c89da295d524b1b5ed2d24670ae5a2
Baokker <1048098862@qq.com>
2022-08-19 11:33:49 +0800

    modify tabs

--- src/components/BottomFooter.vue
+++ src/components/BottomFooter.vue
@@ -6,11 +6,9 @@
       content="疫知通——防疫信息管理系统系统 同济大学2022年暑期数据库课程设计项目"
       placement="bottom"
     >
-          <el-link type="info" 
-      >关于</el-link
-    >
+      <el-link type="info">关于</el-link>
     </el-tooltip>
-    <span style="color: grey">  |  </span>
+    <span style="color: grey"> | </span>
     <el-link type="info" href="https://github.com/KKKA404/DB_Design_Project"
       >GitHub</el-link
     >
