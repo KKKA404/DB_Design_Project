940725ed623ee3f22fec6c8cc7135eda23a83880
Dr-IceCream <90258388+Dr-IceCream@users.noreply.github.com>
2022-08-23 18:18:31 +0800

    Add files via upload

---
+++ src/views/isolationAllocate.vue
@@ -0,0 +1,193 @@
+<template>
+  <div>
+    <el-table :data="searchData" border style="width: 100%">
+      <el-table-column fixed prop="id" label="编号" sortable width="100">
+      </el-table-column>
+      <el-table-column prop="name" label="姓名" width="100"> </el-table-column>
+      <el-table-column prop="sex" label="性别" width="50"> </el-table-column>
+      <el-table-column prop="urgency" label="紧急程度" width="100">
+      </el-table-column>
+      <el-table-column prop="phonenum" label="手机号码" width="120">
+      </el-table-column>
+      <el-table-column prop="IDcard" label="身份证号码" width="240">
+      </el-table-column>
+      <el-table-column prop="location" label="当前住址" width="240">
+      </el-table-column>
+      <el-table-column label="健康状况" prop="health" width="80">
+      </el-table-column>
+      <el-table-column prop="content" label="备注" width="240">
+      </el-table-column>
+      <el-table-column width="120" label="操作">
+        <template>
+          <el-button @click="dialogFormVisible = true" type="text" size="medium"
+            >分配隔离点</el-button
+          >
+        </template>
+      </el-table-column>
+    </el-table>
+
+    <div class="block">
+      <el-dialog title="隔离点分配" :visible.sync="dialogFormVisible" slot>
+        <el-form :model="Emp" ref="Emp">
+          <el-table :data="searchIs" border style="width: 100%">
+            <el-table-column
+              fixed
+              prop="is_id"
+              label="编号"
+              sortable
+              width="100"
+            >
+            </el-table-column>
+            <el-table-column prop="is_phonenum" label="联系号码" width="120">
+            </el-table-column>
+            <el-table-column prop="is_location" label="地址" width="240">
+            </el-table-column>
+            <el-table-column prop="is_capacity" label="总容量" width="100">
+            </el-table-column>
+            <el-table-column prop="is_allowance" label="剩余容量" width="100">
+            </el-table-column>
+            <el-table-column width="120" label="操作">
+              <template slot-scope="scope">
+                <el-button
+                  @click="(dialogFormVisible = true), allocate(scope.row)"
+                  type="text"
+                  size="medium"
+                  >确定分配</el-button
+                >
+              </template>
+            </el-table-column>
+          </el-table>
+        </el-form>
+      </el-dialog>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    allocate(row) {
+      this.$confirm(
+        "确定将当前病人分配到" + row.is_id + "号隔离点?",
+        "删除数据",
+        {
+          confirmButtonText: "确定",
+          cancelButtonText: "取消",
+          type: "warning",
+        }
+      ).then(() => {
+        axios
+          .delete("http://localhost:8080/emp/deleteById/" + row.is_id)
+          //具体逻辑待完成
+          .then((resp) => {
+            this.$alert(row.name + "隔离点分配成功！", "消息", {
+              confirmButtonText: "确定",
+              callback: (action) => {
+                window.location.reload();
+              },
+            });
+          });
+      });
+    },
+  },
+  created() {},
+
+  data() {
+    return {
+      fake_patients_data: [
+        {
+          id: "000001",
+          name: "大贤三赛",
+          sex: "男",
+          urgency: "紧急",
+          phonenum: "18743379266",
+          IDcard: "222543345352383946",
+          location: "同济大学四平路校区",
+          health: "重症",
+          content: "请优先处理",
+        },
+        {
+          id: "000006",
+          name: "大贤老师",
+          sex: "男",
+          urgency: "一般",
+          phonenum: "18723379266",
+          IDcard: "222457345352383946",
+          location: "同济大学嘉定校区",
+          health: "无症状",
+          content: "无",
+        },
+      ],
+
+      fake_isolations_data: [
+        {
+          is_id: "000001",
+          is_phonenum: "18743379266",
+          is_location: "同济大学四平路校区",
+          is_capacity: "100",
+          is_allowance: "50",
+        },
+        {
+          is_id: "000002",
+          is_phonenum: "18723379266",
+          is_location: "同济大学嘉定校区",
+          is_capacity: "200",
+          is_allowance: "20",
+        },
+      ],
+
+      value: "",
+      cname: "",
+      nameInput: "",
+      //searchKey:"",
+      options: [],
+      // value: [],
+      list: [],
+      loading: false,
+      tableData: null,
+      total: null,
+      dialogTableVisible: false,
+      dialogFormVisible: false,
+      formLabelWidth: "120px",
+      LabelWidth: "180px",
+
+      Emp: {
+        id: "",
+        name: "",
+        sex: "",
+        phonenum: "",
+        temp: "",
+        risk: "",
+        health: "",
+        content: "",
+        depart: "",
+      },
+    };
+  },
+  computed: {
+    searchData: function () {
+      let SearchResult = this.fake_patients_data.filter(
+        (item) => String(item.name).indexOf(String(this.nameInput)) > -1
+      );
+      return SearchResult;
+    },
+    searchIs: function () {
+      let SearchResult = this.fake_isolations_data.filter(
+        (item) => String(item.name).indexOf(String(this.nameInput)) > -1
+      );
+      return SearchResult;
+    },
+  },
+};
+</script>
+<style>
+.el-select .el-input {
+  width: 130px;
+}
+.input-with-select .el-input-group__prepend {
+  background-color: #fff;
+}
+.red {
+  color: red;
+}
+</style>
