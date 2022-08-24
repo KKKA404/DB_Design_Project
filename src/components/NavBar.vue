8bcdf7a4742f50f588435d846554d7f4652de87d
Baokker <1048098862@qq.com>
2022-08-24 14:56:46 +0800

    add foldable sidebar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="navbar">
+    <svg-icon
+      :icon-class="this.isCollapse ? 'Collapse' : 'Expand'"
+      @click.native.prevent="changeCollapse"
+    />
+    <span>&nbsp;&nbsp;</span>
     <el-breadcrumb separator="/" class="breadcrumb-container">
       <transition-group name="breadcrumb">
         <el-breadcrumb-item
@@ -15,7 +20,12 @@
     <!-- 要点：在el-menu中设router为true，此后该menu中的item会以自身index为路径跳转 -->
     <div class="right-menu">
       <el-menu :router="true" default-active="$route.path" mode="horizontal">
+        <el-submenu>
+    <template slot="title"><i class="el-icon-user"></i>账户</template>
+        <el-menu-item @click="changePassWord">更改密码</el-menu-item>
+
         <el-menu-item @click="logout">登出</el-menu-item>
+        </el-submenu>
       </el-menu>
     </div>
   </div>
@@ -24,6 +34,11 @@
 <script>
 export default {
   name: "NavBar",
+  data() {
+    return {
+      isCollapse: false,
+    };
+  },
   computed: {
     breadCrumbList() {
       return this.$route.matched;
@@ -33,24 +48,32 @@ export default {
     async logout() {
       await this.$store.dispatch("user/logout");
       this.$message({
-        message: '已登出!',
-        type: 'warning',
-      })
+        message: "已登出!",
+        type: "warning",
+      });
       this.$router.push("/");
     },
+    changeCollapse() {
+      this.isCollapse = !this.isCollapse;
+      this.$emit("changeMenu");
+    },
+    changePassWord() {
+    }
   },
 };
 </script>
 <style>
 .navbar {
   height: 50px;
+  display: flex;
+  justify-content: left;
+  align-items: center;
 }
 
 .right-menu {
-  float: right;
-  line-height: 50px;
+  justify-content: flex-end;
+  margin-left: auto;
 }
-
 .breadcrumb-container {
   float: left;
   height: 100%;
--- src/views/SystemIndex.vue
+++ src/views/SystemIndex.vue
@@ -1,8 +1,13 @@
 <template>
-  <div class="index">
+  <div>
     <el-container style="height: 100%; border: 1px solid #eee">
-      <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
-        <el-menu router :default-active="$route.path">
+      <el-aside width="collapse" style="background-color: #ffffff height: 100%;">
+        <el-menu
+          router
+          :default-active="$route.path"
+          :collapse="isCollapse"
+          class="el-menu-vertical"
+        >
           <el-submenu
             v-for="(item, index) in router"
             :index="index + ''"
@@ -13,26 +18,30 @@
                 v-if="item.meta && item.meta.icon"
                 :icon-class="item.meta && item.meta.icon"
               ></svg-icon>
-              {{ item.name }}</template
+              <span>{{ item.name }}</span></template
             >
-            <el-menu-item
-              v-for="(item2, index2) in item.children"
-              :index="item2.path"
-              :key="index2"
-            >
-              <svg-icon
-                v-if="item2.meta && item2.meta.icon"
-                :icon-class="item2.meta && item2.meta.icon"
-              ></svg-icon>
-              {{ item2.name }}
-            </el-menu-item>
+            <el-menu-item-group>
+              <el-menu-item
+                v-for="(item2, index2) in item.children"
+                :index="item2.path"
+                :key="index2"
+              >
+                <template>
+                  <svg-icon
+                    v-if="item2.meta && item2.meta.icon"
+                    :icon-class="item2.meta && item2.meta.icon"
+                  ></svg-icon>
+                  <span>{{ item2.name }}</span>
+                </template>
+              </el-menu-item>
+            </el-menu-item-group>
           </el-submenu>
         </el-menu>
       </el-aside>
 
       <el-container>
         <el-header>
-          <NavBar></NavBar>
+          <NavBar @changeMenu="changeMenu"></NavBar>
         </el-header>
         <transition name="fade-transform" mode="out-in">
           <router-view />
@@ -45,11 +54,15 @@
   </div>
 </template>
 <script>
+import SvgIcon from "@/components/SvgIcon.vue";
 export default {
+  name: "SystemIndex",
+  components: { SvgIcon },
   data() {
     return {
       currentMenu: "",
       router: "",
+      isCollapse: true,
     };
   },
   created() {
@@ -57,6 +70,29 @@ export default {
       return i.meta;
     });
   },
-  name: "SystemIndex",
+  methods: {
+    changeMenu() {
+      this.isCollapse = !this.isCollapse;
+    },
+  },
 };
 </script>
+
+<style>
+.el-menu-vertical:not(.el-menu--collapse) {
+  width: 200px;
+  overflow: hidden;
+}
+
+.el-menu--collapse {
+  .el-submenu {
+    span {
+      height: 0;
+      width: 0;
+      overflow: hidden;
+      visibility: hidden;
+      display: inline-block;
+    }
+  }
+}
+</style>
