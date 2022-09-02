a4735586a1df6e59ae4eeb2ab053d4169c3ec923
KKKA404 <1738191864@qq.com>
2022-09-02 16:37:00 +0800

    fix params bug

--- src/views/RecordManage.vue
+++ src/views/RecordManage.vue
@@ -241,7 +241,7 @@ export default {
     },
   },
   created() {
-    getPersonalRequest(this.$store.getters.ID).then((res) => {
+    getPersonalRequest({ID:this.$store.getters.ID}).then((res) => {
       this.personalRequest = res.data.personalRequest;
     });
   },
